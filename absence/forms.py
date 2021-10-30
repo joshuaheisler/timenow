91ee3486ee5fd44500f82ca8686afc7bc12be55e
Joshua Heisler <joshua.heisler@stud.th-deg.de>
2021-10-30 14:08:01 +0200

    Anpasssungen

--- absence/forms.py
+++ absence/forms.py
@@ -7,11 +7,19 @@ class AbsenceCreateForm(forms.ModelForm):
         model = Absence
         fields = ['comment', 'absence_from', 'absence_to']
 
+
 class AbsenceDetailForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AbsenceDetailForm, self).__init__(*args, **kwargs)
+        self.fields['absence_from'].disabled = True
+        self.fields['absence_to'].disabled = True
+        self.fields['approved'].disabled = True
+        self.fields['approved_by'].disabled = True
+        self.fields['approved_at'].disabled = True
+        self.fields['approved_by_comment'].disabled = True
         self.fields['comment'].disabled = True
 
     class Meta:
         model = Absence
         fields = '__all__'
+        exclude = ['user']
--- absence/models.py
+++ absence/models.py
@@ -5,14 +5,14 @@ from django.db import models
 
 
 class Absence(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_user', verbose_name='Benutzer', default=None)
-    comment = models.CharField(max_length=300, default=None, verbose_name='Kommentar')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_user', verbose_name='Benutzer')
+    comment = models.CharField(max_length=300, blank=True, null=True, verbose_name='Kommentar')
     absence_from = models.DateField(verbose_name='Abwesend von')
     absence_to = models.DateField(verbose_name='Abwesend bis')
     approved = models.BooleanField(default=None, verbose_name='Genehmigt', null=True)
     approved_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_approver', verbose_name='Genehmiger', null=True)
     approved_at = models.DateTimeField(default=datetime.now, verbose_name='Genehmigt am', null=True)
-    approved_by_comment = models.CharField(max_length=300, default=None, verbose_name='Kommentar des Genehmigers')
+    approved_by_comment = models.CharField(max_length=300, blank=True, null=True, verbose_name='Kommentar des Genehmigers')
 
     @property
     def amount_of_days(self):
